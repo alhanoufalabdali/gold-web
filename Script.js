def51bac64e556c03cb7e6aa02ca348aa2db7d27
Alhanouf Alabdali <hnomoon2009@gmail.com>
2026-05-31 01:43:54 +0300

    first commit

---
+++ Script.js
@@ -0,0 +1,14 @@
+console.log("let's start");
+
+let sumArrow = (a,b) => {
+
+    return a+b
+}
+
+let printText = text => {
+
+    console.log(text);
+}
+
+printText("hnoufa");
+console.log(sumArrow(2,4));
